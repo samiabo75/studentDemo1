66f13a089dae67462ab8046abdbdc30451b22532
Yousef Ballan <ballan_yousef@yahoo.com>
2020-11-16 18:50:21 +0200

    version 1- REQ

--- app/src/main/java/yb/it/studentdemo1/MainActivity.java
+++ app/src/main/java/yb/it/studentdemo1/MainActivity.java
@@ -11,4 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //REQ 01
+    //write code to demonstrate that you are working with Point2D
 }
---
+++ app/src/main/java/yb/it/studentdemo1/Point2d.java
@@ -0,0 +1,11 @@
+package yb.it.studentdemo1;
+
+public class Point2d {
+    public int x;
+    public int y;
+
+    public Point2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
